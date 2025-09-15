529dc529731204ffac6d32337332761853689210
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-09-15 18:57:07 +0300

    make build

---
+++ index.157c4846.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper");
+//# sourceMappingURL=index.157c4846.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,5 @@
+"use strict";
+// eslint-disable-next-line no-new
+new Swiper(".swiper");
+
+//# sourceMappingURL=index.f75de5e1.js.map
